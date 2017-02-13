9cf3db571aa0a0cac7a874a8d0b9ccbaadd850b4
Kiritoc <snake_eats@hotmail.com>
2017-02-13 19:05:11 +0100

    Fix JS error when no navbar

--- web/wp-content/themes/stephanie/assets/js/main.js
+++ web/wp-content/themes/stephanie/assets/js/main.js
@@ -29,36 +29,40 @@
         });
 
         // Keep navbar to the top
-        var $navbar = $("#navbar"),
-            yPos = $navbar.offset().top,
-            height = $navbar.height();
+        var $navbar = $("#navbar");
 
-        $('.js-navbar-container').css({'min-height': height});
+        // Only if navbar exists
+        if ($navbar.length == 1) {
+            var yPos = $navbar.offset().top,
+                height = $navbar.height();
 
-        $(document).scroll(function () {
-            var scrollTop = $(this).scrollTop();
+            $('.js-navbar-container').css({'min-height': height});
 
-            if (scrollTop > yPos) {
-                $navbar.css({"position": "fixed", "top": 0});
-            } else if (scrollTop <= yPos) {
-                $navbar.css({"position": "absolute", "top": 0});
-            }
-        });
+            $(document).scroll(function () {
+                var scrollTop = $(this).scrollTop();
 
-        // Scroll on click
-        $('a.js-scroll-to-href').each(function () {
-            $(this).click(function (event) {
-                var $target = $($.attr(this, 'href'));
-                if ($target.length == 1) {
-                    event.preventDefault();
-                    console.log($target.offset().top);
-
-                    $('html, body').animate({
-                        scrollTop: $target.offset().top
-                    }, 500);
+                if (scrollTop > yPos) {
+                    $navbar.css({"position": "fixed", "top": 0});
+                } else if (scrollTop <= yPos) {
+                    $navbar.css({"position": "absolute", "top": 0});
                 }
             });
-        });
+
+            // Scroll on click
+            $('a.js-scroll-to-href').each(function () {
+                $(this).click(function (event) {
+                    var $target = $($.attr(this, 'href'));
+                    if ($target.length == 1) {
+                        event.preventDefault();
+                        console.log($target.offset().top);
+
+                        $('html, body').animate({
+                            scrollTop: $target.offset().top
+                        }, 500);
+                    }
+                });
+            });
+        }
 
         // Sakura effect
         var $sakura = $('.js-sakura');
