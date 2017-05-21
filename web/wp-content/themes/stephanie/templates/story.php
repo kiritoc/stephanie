c5808d1584f9131c61e33a3caa72c5844dd92adc
Kiritoc <snake_eats@hotmail.com>
2017-05-21 15:24:37 +0200

    Update font + CSS + add our story page

---
+++ web/wp-content/themes/stephanie/templates/story.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Template Name: Notre histoire
+ */
+
+
+get_header();
+if (!get_theme_mod('show_only_main_header')): ?>
+
+    <div id="content">
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="entry-content-page">
+                <?php the_content(); ?>
+
+                <div class="our-timeline-story">
+                    <div class="line"></div>
+                    <div class="timeline-content">
+                        <span class="year">2008</span>
+
+                        <div class="odd post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Première rencontre</h2>
+                                    <p>C’est la rentrée pour les premières années en DUT Informatique à l’IUT d’Arles.
+                                        Environ 50 élèves vont occuper les bancs de l’école. Parmi eux un garçon
+                                        originaire de Toulon, Guillaume Goulet, et une fille originaire de Miramas,
+                                        Stéphanie Bellone. Faisant partie du même groupe de copains, ils vont commencer
+                                        à se côtoyer régulièrement.</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date"><span>Septembre</span></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+
+                        <div class="even post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Premier bisou</h2>
+                                    <p>L’intérêt de l’un pour l’autre commence à se voir. David, un ami de Guillaume,
+                                        décide de jouer les entremetteurs et les invite tous les deux à une soirée à
+                                        côté de Toulon. C’est non sans un certain stress qu’ils entamèrent les deux
+                                        heures de routes qui sépare Arles de Toulon. De cette soirée découlera
+                                        l’histoire d’amour que l’on connait maintenant.</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date"><span>Entre le 13 et le 14 décembre</span></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+
+                        <div class="odd post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Premier album</h2>
+                                    <p>De l’époque de l’IUT d’Arles et de leur rencontre, Stéphanie et Guillaume gardent
+                                        des musiques en tête, celles de l’album de Louise Attaque</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date empty"></div>
+                                <div class="clear"></div>
+                                <div class="wrapper">
+                                    <iframe src='https://embed.spotify.com/?uri=spotify:album:6ASu7ufzcJkzZ9lLfiTyMN'
+                                            frameborder='0' allowtransparency='true'></iframe>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+
+                        <span class="year">2009</span>
+
+                        <div class="even post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Premier appartement</h2>
+                                    <p>Habitant dans la même résidence, logeant déjà quasiment ensemble depuis un petit
+                                        moment, Stéphanie quitta officiellement son appartement pour habiter dans celui
+                                        de Guillaume, un studio de 23 m2 dans la résidence Les Cyclades, à côté de l’IUT
+                                        d’Arles.</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date"><span>Juin</span></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+
+                        <div class="odd post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Première série</h2>
+                                    <p>Fan de la série Friends depuis son enfance, Guillaume la regardait très souvent.
+                                        A force de la voir, et parce qu’elle plait également énormément à Stéphanie,
+                                        cette série est devenu leur série commune préférée à tous les deux.</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date empty"></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+
+                        <span class="year">2011</span>
+
+                        <div class="even post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Première séparation</h2>
+                                    <p>Stéphanie décide de monter à Paris pour continuer ses études. Guillaume resta
+                                        dans le sud pour continuer les siennes à Marseille. Ainsi eu lieu environ un an
+                                        de relation à distance. L’année d’après Guillaume décida de poursuivre ses
+                                        études dans le Nord et rejoignit Stéphanie à Paris.</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date"><span>Septembre</span></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+
+                        <span class="year">2014</span>
+
+                        <div class="odd post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Premier grand voyage</h2>
+                                    <p>Premier voyage aussi loin pour tous les deux. Guillaume et Stéphanie s’envolèrent
+                                        pour 3 semaines aux Etats-Unis. Le voyage débuta par New-York, puis Las Vegas,
+                                        suivi d’un road-trip en voiture jusqu’à San Francisco en passant par Los
+                                        Angeles, le Grand Canyon, la route 66, la Death Valley et bien d’autres. C’est à
+                                        ce moment que germa une certaine idée de mariage dans l’esprit de Guillaume.</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date">Octobre</div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+
+                        <span class="year">2015</span>
+
+                        <div class="even post">
+                            <div class="post-content bounce-in">
+                                <div class="box">
+                                    <h2>Première demande en mariage
+                                        <span class="subtitle">(et dernière  <span
+                                                    class="icon-happy-face"></span>)</span>
+                                    </h2>
+                                    <p>Guillaume fit sa demande en mariage à Stéphanie. Comme vous le savez maintenant,
+                                        le mariage aura lieu le 16 septembre 2017 en Arles, là où tout à commencer.
+                                        C’est avec grand plaisir que l’on espère vous compter parmi nos invités lors de
+                                        cette journée si particulière.</p>
+                                </div>
+                            </div>
+                            <div class="data">
+                                <div class="date"><span>14 décembre</span></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php
+        endwhile;
+        wp_reset_query(); //resetting the page query
+        ?>
+    </div>
+
+    <?php
+endif;
+get_footer(); ?>
